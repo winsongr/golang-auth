6c90504f76dc21ea378cdd222f776f67434e13c1
Winson G <96694861+winsongr@users.noreply.github.com>
2022-03-06 17:36:46 +0530

    first

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
-	"log"
-
+	"fmt"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
 )
 
 func main() {
+	db, err := gorm.Open(mysql.Open("root:root@123@/goreact"), &gorm.Config{})
+	if err != nil {
+		panic("error db connetion")
+	}
+	fmt.Println(db)
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
