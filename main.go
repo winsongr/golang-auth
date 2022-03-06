4f2a75111600b2b22d43f8f1453ab31487209e4b
Winson G <96694861+winsongr@users.noreply.github.com>
2022-03-06 17:11:06 +0530

    1

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
 
-func main(){
-	fmt.Println("hi")
-} 
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	log.Fatal(app.Listen(":3000"))
+}
